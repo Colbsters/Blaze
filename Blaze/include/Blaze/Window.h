482ab87a874962057e68954ae741228889edcb52
Colby Salkeld <csalkeld0507@gmail.com>
2021-07-05 10:39:24 -0500

    Added new functions to Window base class. Blaze project now uses C++17.

--- Blaze/include/Blaze/Window.h
+++ Blaze/include/Blaze/Window.h
@@ -6,6 +6,10 @@
 #include <Blaze/Core.h>
 #include <Blaze/Object.h>
 
+#include <string>
+#include <string_view>
+#include <array>
+
 namespace Blaze
 {
 	namespace Details
@@ -68,17 +72,32 @@ namespace Blaze
 		// Removes an event handler
 		inline Result RemoveEventHandler(WindowEvent::EventCode eventCode, WindowEventHandler eventHandler) { return RemoveEventHandler_Impl(eventCode, eventHandler); }
 
+		// Sets te window title
+		inline Result SetTitle(std::string_view newTitle) { return SetTitle_Impl(newTitle); }
+		// Gets the window title
+		inline std::string GetTitle() { return GetTitle_Impl(); }
 		// Resizes the window
 		inline Result Resize(uint32_t width, uint32_t height) { return Resize_Impl(width, height); }
+		// Gets the window size
+		inline std::array<uint32_t, 2> GetWindowSize() { return GetWindowSize_Impl(); }
+		// Gets the window client (renderable area) size
+		inline std::array<uint32_t, 2> GetClientSize() { return GetClientSize_Impl(); }
 		// Moves the window
 		inline Result Move(int32_t x, int32_t y) { return Move_Impl(x, y); }
+		// Gets the position of the window
+		inline std::array<int32_t, 2> GetPosition() { return GetPosition_Impl(); }
 	private:
 		virtual Result Update_Impl() = 0;
 		virtual bool IsRunning_Impl() = 0;
 		virtual Result PushEventHandler_Impl(WindowEvent::EventCode eventCode, WindowEventHandler eventHandler) = 0;
 		virtual Result RemoveEventHandler_Impl(WindowEvent::EventCode eventCode, WindowEventHandler eventHandler) = 0;
+		virtual Result SetTitle_Impl(std::string_view) = 0;
+		virtual std::string GetTitle_Impl() = 0;
 		virtual Result Resize_Impl(uint32_t width, uint32_t height) = 0;
+		virtual std::array<uint32_t, 2> GetWindowSize_Impl() = 0;
+		virtual std::array<uint32_t, 2> GetClientSize_Impl() = 0;
 		virtual Result Move_Impl(int32_t x, int32_t y) = 0;
+		virtual std::array<int32_t, 2> GetPosition_Impl() = 0;
 	};
 }
 
